099dcc988901521f630555ec3b1717cf0dd0cf66
muzikbike <52297970+muzikbike@users.noreply.github.com>
2024-04-11 15:47:39 +0100

    first test

--- 1.20.5/src/main/java/com/mojang/slicer/Main.java
+++ 1.20.5/src/main/java/com/mojang/slicer/Main.java
@@ -45,10 +45,6 @@ public class Main {
         return new InputFile(path).outputs(outputs);
     }
 
-    private static InputFile move(final String inputPath, final String outputPath) {
-        return new InputFile(inputPath).outputs(new OutputFile(outputPath, new Box(0, 0, 1, 1, 1, 1)));
-    }
-
     private static final List<InputFile> INPUTS = List.of(
         input("assets/minecraft/textures/map/map_icons.png",
             mapDecoration("player", 0),
@@ -108,6 +104,9 @@ public class Main {
             moonPhase("moon_waxing_crescent", 5),
             moonPhase("moon_first_quarter", 6),
             moonPhase("moon_waxing_gibbous", 7)
+        ),
+        input("assets/minecraft/textures/environment/sun.png",
+            new OutputFile("assets/minecraft/textures/celestial_body/sun.png", new Box(0, 0, 1, 1, 1, 1))
         )
     );
 
